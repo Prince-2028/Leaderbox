a820b057e42fcbd5533b2a3fe65818a62de3ad1e
prince <pk20287022@gmail.com>
2025-07-14 22:05:24 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import AddUser from "./components/AddUser";
+const App = () => {
+  return (
+     <h1 className="text-3xl font-bold underline">
+    Hello world!
+    <AddUser/>
+  </h1>
+  )
+};
+
+export default App;
---
+++ frontend/src/components/ClaimButton.jsx
@@ -0,0 +1,14 @@
+// ClaimButton.js
+// This component will be a button to claim something (e.g., a reward).
+
+import React from 'react';
+
+const ClaimButton = () => {
+  return (
+    <button>
+      Claim
+    </button>
+  );
+};
+
+export default ClaimButton;
