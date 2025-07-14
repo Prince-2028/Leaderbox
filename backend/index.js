3c8cde5f8a98522af80f9b139418a78e7bcfeb4b
prince <pk20287022@gmail.com>
2025-07-14 22:41:57 +0530

    monodb connection

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
